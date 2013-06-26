5767d9221ce384dd664a7bbe5a8e70418f9a5152
shoo <zan77137@nifty.com>
2013-06-27 03:49:27 +0900

    JSON編集用API追加

--- voile/misc.d
+++ voile/misc.d
@@ -3,11 +3,12 @@
  */
 module voile.misc;
 
-import core.memory, core.thread, core.exception;
+import core.memory, core.thread, core.exception, core.sync.mutex;
 import std.concurrency, std.parallelism;
 import std.stdio, std.exception, std.conv, std.string, std.variant;
 import std.range, std.container, std.array, std.typetuple;
 import std.functional, std.typecons, std.traits, std.typetuple;
+import std.json;
 
 
 /* This template based from std.typecons.MemberFunctionGenerator */
@@ -388,6 +389,7 @@ private:
 		}
 	}
 	Node* root;
+	Mutex _mutex;
 public:
 	/***************************************************************************
 	 * 
@@ -590,7 +592,7 @@ public:
 		}
 	}
 	
-	this()
+	this() pure
 	{
 		static if (__traits(compiles, { T v; }))
 		{
@@ -607,6 +609,7 @@ public:
 		root = new Node(v, null, null);
 		root.next = root;
 		root.prev = root;
+		_mutex = new Mutex(this);
 	}
 	
 	@property bool empty() const pure nothrow
@@ -884,7 +887,6 @@ private:
 	alias typeof(&DummyData.init.emit) Proc;
 	alias _exFuncInfo.PT Args;
 	alias List!Proc ProcList;
-	ProcList.Range end;
 	ProcList _procs;
 public:
 	/***************************************************************************
@@ -1021,7 +1023,21 @@ public:
 		_procs.clear();
 	}
 	
+	/***************************************************************************
+	 * 
+	 */
+	void lock() shared
+	{
+		(cast()_procs._mutex).lock();
+	}
 	
+	/***************************************************************************
+	 * 
+	 */
+	void unlock() shared
+	{
+		(cast()_procs._mutex).unlock();
+	}
 }
 
 
@@ -2266,6 +2282,7 @@ private S indentCtfe(S)(S s, S indentStr = " ")
 	return app.data;
 }
 
+
 /*******************************************************************************
  * 
  */
@@ -2301,3 +2318,608 @@ unittest
 		>`);
 }
 
+/*******************************************************************************
+ * JSONValueデータを得る
+ */
+JSONValue json(T)(T[] x) @property /+pure+/
+	if (isSomeString!(T[]))
+{
+	JSONValue v;
+	v.type = JSON_TYPE.STRING;
+	v.str = to!string(x);
+	return v;
+}
+
+///
+unittest
+{
+	dstring dstr = "あいうえお";
+	wstring wstr = "かきくけこ";
+	string  str  = "さしすせそ";
+	auto dstrjson = dstr.json;
+	auto wstrjson = wstr.json;
+	auto strjson  = str.json;
+	assert(dstrjson.type == JSON_TYPE.STRING);
+	assert(wstrjson.type == JSON_TYPE.STRING);
+	assert(strjson.type  == JSON_TYPE.STRING);
+	assert(dstrjson.str == "あいうえお");
+	assert(wstrjson.str == "かきくけこ");
+	assert(strjson.str  == "さしすせそ");
+}
+
+/// ditto
+JSONValue json(T)(T x) @property /+pure+/
+	if (isIntegral!T && !is(T == enum))
+{
+	JSONValue v;
+	v.type = JSON_TYPE.INTEGER;
+	v.integer = x;
+	return v;
+}
+
+///
+unittest
+{
+	import std.typetuple;
+	foreach (T; TypeTuple!(ubyte, byte, ushort, short, uint, int, ulong, long))
+	{
+		T x = 123;
+		auto xjson = x.json;
+		assert(xjson.type == JSON_TYPE.INTEGER);
+		assert(xjson.integer == 123);
+	}
+}
+
+
+/// ditto
+JSONValue json(T)(T x) @property /+pure+/
+	if (isFloatingPoint!T)
+{
+	JSONValue v;
+	v.type = JSON_TYPE.FLOAT;
+	v.floating = x;
+	return v;
+}
+
+///
+unittest
+{
+	import std.typetuple;
+	foreach (T; TypeTuple!(float, double, real))
+	{
+		T x = 0.125;
+		auto xjson = x.json;
+		assert(xjson.type == JSON_TYPE.FLOAT);
+		assert(xjson.floating == 0.125);
+	}
+}
+
+
+/// ditto
+JSONValue json(T)(T[] ary) @property /+pure+/
+	if (!isSomeString!(T[]) && isArray!(T[]))
+{
+	auto app = appender!(JSONValue[])();
+	JSONValue v;
+	foreach (x; ary)
+	{
+		app.put(x.json);
+	}
+	v.type = JSON_TYPE.ARRAY;
+	v.array = app.data;
+	return v;
+}
+
+///
+unittest
+{
+	auto ary = [1,2,3];
+	auto aryjson = ary.json;
+	assert(aryjson.type == JSON_TYPE.ARRAY);
+	assert(aryjson[0].type == JSON_TYPE.INTEGER);
+	assert(aryjson[1].type == JSON_TYPE.INTEGER);
+	assert(aryjson[2].type == JSON_TYPE.INTEGER);
+	assert(aryjson[0].integer == 1);
+	assert(aryjson[1].integer == 2);
+	assert(aryjson[2].integer == 3);
+}
+
+///
+unittest
+{
+	auto ary = ["ab","cd","ef"];
+	auto aryjson = ary.json;
+	assert(aryjson.type == JSON_TYPE.ARRAY);
+	assert(aryjson[0].type == JSON_TYPE.STRING);
+	assert(aryjson[1].type == JSON_TYPE.STRING);
+	assert(aryjson[2].type == JSON_TYPE.STRING);
+	assert(aryjson[0].str == "ab");
+	assert(aryjson[1].str == "cd");
+	assert(aryjson[2].str == "ef");
+}
+
+///
+unittest
+{
+	struct A
+	{
+		int a = 123;
+		JSONValue json() const @property
+		{
+			JSONValue v;
+			v.type = JSON_TYPE.OBJECT;
+			v.setValue("a", a);
+			return v;
+		}
+		void json(JSONValue v) @property
+		{
+			enforce(v.type == JSON_TYPE.OBJECT);
+			a = v.getValue("a", 123);
+		}
+	}
+	auto ary = [A(1),A(2),A(3)];
+	auto aryjson = ary.json;
+	assert(aryjson.type == JSON_TYPE.ARRAY);
+	assert(aryjson[0].type == JSON_TYPE.OBJECT);
+	assert(aryjson[1].type == JSON_TYPE.OBJECT);
+	assert(aryjson[2].type == JSON_TYPE.OBJECT);
+	assert(aryjson[0]["a"].type == JSON_TYPE.INTEGER);
+	assert(aryjson[1]["a"].type == JSON_TYPE.INTEGER);
+	assert(aryjson[2]["a"].type == JSON_TYPE.INTEGER);
+	assert(aryjson[0]["a"].integer == 1);
+	assert(aryjson[1]["a"].integer == 2);
+	assert(aryjson[2]["a"].integer == 3);
+}
+
+
+/// ditto
+JSONValue json(T)(T x) @property /+pure+/
+	if (is(Unqual!T == bool))
+{
+	JSONValue v;
+	if (x)
+	{
+		v.type = JSON_TYPE.TRUE;
+	}
+	else
+	{
+		v.type = JSON_TYPE.FALSE;
+	}
+	return v;
+}
+
+///
+unittest
+{
+	bool bt = true;
+	bool bf = false;
+	auto btjson = bt.json;
+	auto bfjson = bf.json;
+	assert(btjson.type == JSON_TYPE.TRUE);
+	assert(bfjson.type == JSON_TYPE.FALSE);
+}
+
+
+/*******************************************************************************
+ * JSONValueデータの操作
+ */
+void setValue(T)(ref JSONValue v, string name, T val) /+pure+/
+	if (is(typeof(val.json)))
+	body
+{
+	v.object[name] = val.json;
+}
+
+///
+unittest
+{
+	JSONValue json;
+	json.type = JSON_TYPE.OBJECT;
+	json.setValue("dat", 123);
+	assert(json.type == JSON_TYPE.OBJECT);
+	assert("dat" in json.object);
+	assert(json["dat"].type == JSON_TYPE.INTEGER);
+	assert(json["dat"].integer == 123);
+}
+
+
+/// ditto
+void setValue(T)(ref JSONValue v, string name, T val) /+pure+/
+	if (is(T == enum))
+	in { assert(v.type == JSON_TYPE.OBJECT); }
+	body
+{
+	v.object[name] = to!string(val).json;
+}
+
+///
+unittest
+{
+	enum Type
+	{
+		foo, bar,
+	}
+	JSONValue json;
+	json.type = JSON_TYPE.OBJECT;
+	json.setValue("type", Type.foo);
+	assert(json.type == JSON_TYPE.OBJECT);
+	assert("type" in json.object);
+	assert(json["type"].type == JSON_TYPE.STRING);
+	assert(json["type"].str == "foo");
+}
+
+///
+unittest
+{
+	struct A
+	{
+		int a = 123;
+		JSONValue json() const @property
+		{
+			JSONValue v;
+			v.type = JSON_TYPE.OBJECT;
+			v.setValue("a", a);
+			return v;
+		}
+	}
+	A a;
+	a.a = 321;
+	JSONValue json;
+	json.setValue("a", a);
+}
+
+
+/// ditto
+T getValue(T)(ref JSONValue v, string name, T defaultVal = T.init) /+pure+/
+	if (isSomeString!(T))
+{
+	T ret = defaultVal;
+	if (auto x = name in v.object)
+	{
+		if (x.type == JSON_TYPE.STRING)
+		{
+			ret = to!T(x.str);
+		}
+	}
+	return ret;
+}
+
+///
+unittest
+{
+	JSONValue json;
+	json.type = JSON_TYPE.OBJECT;
+	json.setValue("test", "あいうえお");
+	static assert(is(typeof(json.getValue("test", "かきくけこ"d)) == dstring));
+	static assert(is(typeof(json.getValue("test", "かきくけこ"w)) == wstring));
+	static assert(is(typeof(json.getValue("test", "かきくけこ"c)) == string));
+	assert(json.getValue("test", "かきくけこ"d) == "あいうえお"d);
+	assert(json.getValue("test", "かきくけこ"w) == "あいうえお"w);
+	assert(json.getValue("test", "かきくけこ"c) == "あいうえお"c);
+	assert(json.getValue("hoge", "かきくけこ"c) == "かきくけこ"c);
+}
+
+
+/// ditto
+T getValue(T)(ref JSONValue v, string name, T defaultVal = T.init) /+pure+/
+	if (isIntegral!T && !is(T == enum))
+{
+	T ret = defaultVal;
+	if (auto x = name in v.object)
+	{
+		if (x.type == JSON_TYPE.INTEGER)
+		{
+			ret = cast(T)x.integer;
+		}
+	}
+	return ret;
+}
+
+///
+unittest
+{
+	JSONValue json;
+	json.type = JSON_TYPE.OBJECT;
+	json.setValue("test", 123);
+	static assert(is(typeof(json.getValue("test", 654UL)) == ulong));
+	static assert(is(typeof(json.getValue("test", 654U)) == uint));
+	static assert(is(typeof(json.getValue("test", cast(byte)12)) == byte));
+	assert(json.getValue("test", 654UL) == 123);
+	assert(json.getValue("test", 654U) == 123);
+	assert(json.getValue("test", cast(byte)12) == 123);
+	assert(json.getValue("hoge", cast(byte)12) == 12);
+}
+
+/// ditto
+T getValue(T)(ref JSONValue v, string name, T defaultVal = T.init) /+pure+/
+	if (isFloatingPoint!T)
+{
+	T ret = defaultVal;
+	if (auto x = name in v.object)
+	{
+		if (x.type == JSON_TYPE.FLOAT)
+		{
+			ret = x.floating;
+		}
+	}
+	return ret;
+}
+
+///
+unittest
+{
+	JSONValue json;
+	json.type = JSON_TYPE.OBJECT;
+	json.setValue("test", 0.125);
+	static assert(is(typeof(json.getValue("test", 0.25f)) == float));
+	static assert(is(typeof(json.getValue("test", 0.25)) == double));
+	static assert(is(typeof(json.getValue("test", 0.25L)) == real));
+	assert(json.getValue("test", 0.25f) == 0.125f);
+	assert(json.getValue("test", 0.25) == 0.125);
+	assert(json.getValue("test", 0.25L) == 0.125L);
+	assert(json.getValue("hoge", 0.25L) == 0.25L);
+}
+
+/// ditto
+T getValue(T)(ref JSONValue v, string name, T defaultVal = T.init) /+pure+/
+	if (is(T == struct))
+{
+	T ret = defaultVal;
+	if (auto x = name in v.object)
+	{
+		if (x.type == JSON_TYPE.OBJECT)
+		{
+			ret.json = *x;
+		}
+	}
+	return ret;
+}
+
+///
+unittest
+{
+	struct A
+	{
+		int a = 123;
+		JSONValue json() const @property
+		{
+			JSONValue v;
+			v.type = JSON_TYPE.OBJECT;
+			v.setValue("a", a);
+			return v;
+		}
+		void json(JSONValue v) @property
+		{
+			enforce(v.type == JSON_TYPE.OBJECT);
+			a = v.getValue("a", 123);
+		}
+	}
+	A a;
+	a.a = 321;
+	JSONValue json;
+	json.setValue("a", a);
+	static assert(is(typeof(json.getValue("a", A(456))) == A));
+	assert(json.getValue("a", A(456)).a == 321);
+	assert(json.getValue("b", A(456)).a == 456);
+}
+
+
+/// ditto
+T getValue(T)(ref JSONValue v, string name, T defaultVal = T.init) /+pure+/
+	if (is(T == class))
+{
+	T ret = defaultVal;
+	if (auto x = name in v.object)
+	{
+		if (x.type == JSON_TYPE.OBJECT)
+		{
+			ret = new T;
+			ret.json = *x;
+		}
+	}
+	return ret;
+}
+
+///
+unittest
+{
+	static class A
+	{
+		int a = 123;
+		JSONValue json() const @property
+		{
+			JSONValue v;
+			v.type = JSON_TYPE.OBJECT;
+			v.setValue("a", a);
+			return v;
+		}
+		void json(JSONValue v) @property
+		{
+			enforce(v.type == JSON_TYPE.OBJECT);
+			a = v.getValue("a", 123);
+		}
+	}
+	auto a = new A;
+	a.a = 321;
+	JSONValue json;
+	json.setValue("a", a);
+	static assert(is(typeof(json.getValue!A("a")) == A));
+	assert(json.getValue!A("a").a == 321);
+	assert(json.getValue!A("b") is null);
+}
+
+
+/// ditto
+T getValue(T)(ref JSONValue v, string name, T defaultVal = T.init) /+pure+/
+	if (is(T == enum))
+{
+	T ret = defaultVal;
+	if (auto x = name in v.object)
+	{
+		if (x.type == JSON_TYPE.STRING)
+		{
+			ret = to!T(x.str);
+		}
+	}
+	return ret;
+}
+
+///
+unittest
+{
+	enum Type
+	{
+		foo, bar
+	}
+	JSONValue json;
+	json.type = JSON_TYPE.OBJECT;
+	json.setValue("a", Type.bar);
+	static assert(is(typeof(json.getValue("a", Type.foo)) == Type));
+	assert(json.getValue("a", Type.foo) == Type.bar);
+	assert(json.getValue("b", Type.foo) == Type.foo);
+}
+
+
+/// ditto
+T getValue(T)(ref JSONValue v, string name, T defaultVal = T.init) /+pure+/
+	if (is(T == bool))
+{
+	T ret = defaultVal;
+	if (auto x = name in v.object)
+	{
+		if (x.type == JSON_TYPE.TRUE)
+		{
+			ret = true;
+		}
+		else if (x.type == JSON_TYPE.FALSE)
+		{
+			ret = false;
+		}
+	}
+	return ret;
+}
+
+///
+unittest
+{
+	JSONValue json;
+	json.type = JSON_TYPE.OBJECT;
+	json.setValue("t", true);
+	json.setValue("f", false);
+	static assert(is(typeof(json.getValue("t", true)) == bool));
+	static assert(is(typeof(json.getValue("f", false)) == bool));
+	assert(json.getValue("t", true) == true);
+	assert(json.getValue("f", true) == false);
+	assert(json.getValue("t", false) == true);
+	assert(json.getValue("f", false) == false);
+	assert(json.getValue("x", true) == true);
+	assert(json.getValue("x", false) == false);
+}
+
+
+/// ditto
+T getValue(T)(ref JSONValue v, string name, T defaultVal = T.init) /+pure+/
+	if (!isSomeString!(T) && isArray!(T))
+{
+	alias ForeachType!T E;
+	enum ty = E.init.json.type;
+	T ret = defaultVal;
+	auto app = appender!T();
+	if (auto x = name in v.object)
+	{
+		if (x.type == JSON_TYPE.ARRAY)
+		{
+			foreach (e; x.array)
+			{
+				static if (ty == JSON_TYPE.INTEGER)
+				{
+					app ~= to!E(e.integer);
+				}
+				else static if (ty == JSON_TYPE.FLOAT)
+				{
+					app ~= to!E(e.floating);
+				}
+				else static if (ty == JSON_TYPE.STRING)
+				{
+					app ~= to!E(e.str);
+				}
+				else static if (is(E == bool))
+				{
+					app ~= e.type == JSON_TYPE.TRUE;
+				}
+				else static if (ty == JSON_TYPE.OBJECT && is(E == struct) && is(typeof({E o; o.json = e;})))
+				{
+					E o;
+					o.json = e;
+					app ~= o;
+				}
+				else static if (ty == JSON_TYPE.OBJECT && is(E == class) && is(typeof({auto o = new E; o.json = e;})))
+				{
+					auto o = new E;
+					o.json = e;
+					app ~= o;
+				}
+				else
+				{
+					static assert(0);
+				}
+			}
+		}
+	}
+	return app.data.length ? app.data : ret;
+}
+
+///
+unittest
+{
+	JSONValue json;
+	json.type = JSON_TYPE.OBJECT;
+	json.setValue("test1", [1,2,3]);
+	auto x = json.getValue("test1", [2,3,4]);
+	static assert(is(typeof(json.getValue("test1", [2,3,4])) == int[]));
+	assert(json.getValue("test1", [2,3,4]) == [1,2,3]);
+	assert(json.getValue("test1x", [2,3,4]) == [2,3,4]);
+	
+	json.setValue("test2", [0.5,0.25,0.125]);
+	static assert(is(typeof(json.getValue("test2", [0.5,0.5,0.5])) == double[]));
+	assert(json.getValue("test2", [0.5,0.5,0.5]) == [0.5,0.25,0.125]);
+	assert(json.getValue("test2x", [0.5,0.5,0.5]) == [0.5,0.5,0.5]);
+	
+	json.setValue("test3", ["ab","cd","ef"]);
+	static assert(is(typeof(json.getValue("test3", ["あい","うえ","おか"])) == string[]));
+	assert(json.getValue("test3", ["あい","うえ","おか"]) == ["ab","cd","ef"]);
+	assert(json.getValue("test3x", ["あい","うえ","おか"]) == ["あい","うえ","おか"]);
+	
+	json.setValue("test4", [true, false, true]);
+	static assert(is(typeof(json.getValue("test4", [false, true, true])) == bool[]));
+	assert(json.getValue("test4", [false, true, true]) == [true, false, true]);
+	assert(json.getValue("test4x", [false, true, true]) == [false, true, true]);
+}
+
+///
+unittest
+{
+	static struct A
+	{
+		int a = 123;
+		JSONValue json() const @property
+		{
+			JSONValue v;
+			v.type = JSON_TYPE.OBJECT;
+			v.setValue("a", a);
+			return v;
+		}
+		void json(JSONValue v) @property
+		{
+			enforce(v.type == JSON_TYPE.OBJECT);
+			a = v.getValue("a", 123);
+		}
+	}
+	auto a = [A(1),A(2),A(3)];
+	JSONValue json;
+	json.type = JSON_TYPE.OBJECT;
+	json.setValue("a", a);
+	static assert(is(typeof(json.getValue("a", [A(4),A(5),A(6)])) == A[]));
+	assert(json.getValue("a", [A(4),A(5),A(6)]) == [A(1),A(2),A(3)]);
+	assert(json.getValue("b", [A(4),A(5),A(6)]) == [A(4),A(5),A(6)]);
+}
